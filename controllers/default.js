2699869cac8e8d42fd2765423d8ce285a7758bc5
Divyanshu <53134822+dev-idblfs@users.noreply.github.com>
2020-05-05 07:56:15 +0530

    first init

--- controllers/default.js
+++ controllers/default.js
@@ -1,11 +1,175 @@
 var express = require("express"),
     router = express.Router();
+var path = require('path')
+var ejs = require('ejs');
 
+const _loadJS = () => {
+    let js = {
+        a: "public/js/jquery.min.js",
+        b: "public/js/bootstrap.min.js",
+        c: "public/js/slick.min.js",
+        d: "public/js/venobox.min.js",
+        e: "public/js/gmap.js",
+        f: "public/js/fuse.min.js",
+        g: "public/js/mark.js",
+        h: "public/js/search.js",
+        i: "public/js/ddj.js",
+        j: "public/js/script.min.js",
+        k: "https://unpkg.com/filepond/dist/filepond.js",
+        brandIcon: 'public/images/icon/icon.png'
+    }
+    return js;
+}
+
+const _loadCSS = () => {
+    let css = {
+        a: "public/scss/bootstrap.min.css",
+        b: "public/scss/slick.css",
+        c: "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+        d: "public/scss/venobox.css",
+        e: "public/scss/ddj.css",
+        f: "public/scss/style.min.css",
+        g: 'https://unpkg.com/filepond/dist/filepond.css',
+        brandIcon: 'public/images/icon/icon.png',
+        loader: 'public/images/preloader.gif'
+    }
+    return css
+}
 // request to get all the users
-router.get("/", function (req, res) {
-
-    res.redirect('http://localhost:3000/v');
+router.get("/", async function (req, res) {
+    var header = await ejs.renderFile('views/header.ejs', { css: _loadCSS() });
+    var footer = await ejs.renderFile('views/footer.ejs', { js: _loadJS() });
+    var body = await ejs.renderFile('views/index.ejs');
+    res.render('body', { header: header, body: body, footer: footer });
 })
 
 // request to get all the users by userName
+router.get("/about", async function (req, res) {
+    var header = await ejs.renderFile('views/header.ejs', { css: _loadCSS() });
+    var footer = await ejs.renderFile('views/footer.ejs', { js: _loadJS() });
+    var body = await ejs.renderFile('views/about/index.ejs');
+
+    res.render('body', { header: header, body: body, footer: footer });
+
+})
+router.get("/service", async function (req, res) {
+    var header = await ejs.renderFile('views/header.ejs', { css: _loadCSS() });
+    var footer = await ejs.renderFile('views/footer.ejs', { js: _loadJS() });
+    var body = await ejs.renderFile('views/service/index.ejs');
+
+    res.render('body', { header: header, body: body, footer: footer });
+
+})
+router.get("/contact", async function (req, res) {
+    var header = await ejs.renderFile('views/header.ejs', { css: _loadCSS() });
+    var footer = await ejs.renderFile('views/footer.ejs', { js: _loadJS() });
+    var body = await ejs.renderFile('views/contact/index.ejs');
+
+    res.render('body', { header: header, body: body, footer: footer });
+
+})
+
+router.get("/resume", async function (req, res) {
+    var header = await ejs.renderFile('views/header.ejs', { css: _loadCSS() });
+    var footer = await ejs.renderFile('views/footer.ejs', { js: _loadJS() });
+    var body = await ejs.renderFile('views/resume/index.ejs');
+
+    res.render('body', { header: header, body: body, footer: footer });
+
+})
+
+router.post("/resume", async (req, res) => {
+    if (Object.keys(req.body).length > 0) {
+        let params = req.body;
+        console.log(req.files);
+        try {
+            if (params.email && params.step && params.step == 1) {
+                delete params.step;
+                isfound = await resume.fetch({ email: params.email })
+                if (isfound.body.length > 0) {
+                    return res.send({ status: 304, message: "Email Already Exists" });;
+                }
+                params.isVerified = 0;
+                params.otp = Math.floor(100000 + Math.random() * 900000);
+                await mail(params.email, params.otp);
+                result = await resume.insert(params);
+                if (result.status == 200) {
+                    res.send({ status: 200, message: "Varify Your Email" });
+                } else res.send({ status: 200, message: "not Inserted" });
+            } else if (params.otp && params.email && params.step && params.step == 2) {
+                delete params.step;
+                filter = {
+                    email: params.email
+                }
+                let list = await resume.fetch(filter)
+                if (list.body[0].otp == params.otp && list.body[0].email == params.email) {
+                    result = await resume.update({ isVerified: 1 }, { _id: list.body[0]._id });
+                    if (result.status == 200) {
+                        return res.send({ status: 200, message: "Email Varified" });
+                    }
+                } else {
+                    return res.send({ status: 200, message: "Wrong OTP Entered" });
+                }
+            } else if (params.email && params.step && params.step == 3) {
+                delete params.step;
+                filter = {
+                    email: params.email
+                }
+                let list = await resume.fetch(filter)
+                result = await resume.update(params, { _id: list.body[0]._id });
+                if (result.status == 200) {
+                    return res.send({ status: 200, message: "Thankyou For Subbmitted" });
+                }
+                // console.log(params.file);
+                // var s3 = require(ROOT_DIR + "/utils/s3.js")
+
+                // // s3.putObject()
+                // res.send({ status: 200, message: "Thankyou For Subbmitted" });
+            }
+        } catch (error) {
+            result = error;
+            res.send({ status: 304, message: "plases try again" });
+        }
+
+    }
+})
+
+const mail = (email, code) => {
+    return new Promise(async (resolve, reject) => {
+        const options = {
+            method: 'POST',
+            uri: 'https://idblfs-email-sending.herokuapp.com/sendmail',
+            body: {
+                from: "Onxcy <no-reply@onxcy.com>",
+                to: email,
+                subject: "Onxcy - Verify Email",
+                body_html: `Hey ${email.split('@')[0]}!</br></br>
+    
+                <strong>Verify your email - Just one more step!</strong>
+                <br><br>
+                We’re on a mission to let you make chat bots and make your working life simpler, more pleasant and more productive. This should be easy.
+                <br><br>
+                To get started, first you need to verify the email address 
+                <br><br>
+                enter this Verification code on the Verification Page ${code}
+                <br><br>
+                <br><br>
+                
+                Thanks,
+                Onxcy Team`
+            },
+            json: true,
+        }
+        request(options).then((body) => {
+            if (body) {
+                console.log(body);
+                resolve({ status: 200, message: body })
+            }
+        }).catch((err) => {
+            reject({ status: 304, message: "Please try Again" })
+        });
+    });
+
+}
+
 module.exports = router;
---
+++ modals/resumes.js
@@ -0,0 +1,79 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+// const uri = "mongodb+srv://admin_prod:nxcy@123@idblfs-7za2q.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb://127.0.0.1:27017/onxcy_resume";
+// const client = new MongoClient(uri);
+const db_name = 'onxcy_resume';
+const collection = 'resumes';
+const option = { useNewUrlParser: true, useUnifiedTopology: true }
+
+const insert = (params) => {
+    return new Promise((resolve, reject) => {
+        var data = params;
+        MongoClient.connect(uri, option).then(async (db) => {
+            var dbo = db.db(db_name);
+            try {
+                const res = await dbo.collection(collection).insertOne(data);
+                console.log("enterd");
+                resolve({ status: 200, body: res.result })
+            } catch (err) {
+                console.log('not entered');
+                reject({ status: 500, error: err });
+                db.close();
+            }
+        }).catch((err) => {
+            console.log('connt connect')
+            reject({ status: 502, error: err })
+        })
+    });
+}
+
+const fetch = (filter = {}) => {
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(uri, option).then(async (db) => {
+            var dbo = db.db(db_name);
+            try {
+                let result = await dbo.collection(collection).find(filter).toArray()
+                console.log("fetched");
+                resolve({ status: 200, body: result })
+            } catch (err) {
+                console.log('not entered');
+                reject({ status: 500, error: err });
+                db.close();
+            }
+        }).catch((err) => {
+            console.log('connt connect')
+            reject({ status: 502, error: err })
+        })
+    });
+
+}
+
+const update = (params, where) => {
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(uri, option).then(async (db) => {
+            var dbo = db.db(db_name);
+            try {
+                console.log(params);
+                var newdata = { $set: params };
+                let result = await dbo.collection(collection).updateOne(where, newdata)
+                console.log("updated");
+                resolve({ status: 200, body: result })
+            } catch (err) {
+                console.log('not update');
+                reject({ status: 500, error: err });
+                db.close();
+            }
+        }).catch((err) => {
+            console.log('connt connect')
+            reject({ status: 502, error: err })
+        })
+    });
+
+}
+
+module.exports = {
+    insert: insert,
+    fetch: fetch,
+    update: update
+}
