0834a1d20547345b84ec32c1712b93ba692de5af
Divyanshu <53134822+dev-idblfs@users.noreply.github.com>
2020-05-24 05:12:51 +0530

    Update routes.js

--- config/routes.js
+++ config/routes.js
@@ -6,25 +6,25 @@ router.get('/sitemap.xml', function (req, res) {
 });
 
 router.post('/done', (req, res) => {
-    const { spawn } = require('child_process');
-    const bat = spawn('cmd.exe', ['/c', 'bat.sh']);
-    var output = '';
-    bat.stdout.on('data', (data) => {
-        console.log(data.toString());
-        output += data;
-    });
+//     const { spawn } = require('child_process');
+//     const bat = spawn('cmd.exe', ['/c', 'bat.sh']);
+//     var output = '';
+//     bat.stdout.on('data', (data) => {
+//         console.log(data.toString());
+//         output += data;
+//     });
 
-    bat.stderr.on('data', (data) => {
-        console.error(data.toString());
-        // console.log('hello');
-        res.end(data);
-    });
+//     bat.stderr.on('data', (data) => {
+//         console.error(data.toString());
+//         // console.log('hello');
+//         res.end(data);
+//     });
 
-    bat.on('exit', (code) => {
-        console.log(`Child exited with code ${code}`);
-        res.send(output || "ok");
-    });
-
+//     bat.on('exit', (code) => {
+//         console.log(`Child exited with code ${code}`);
+//         res.send(output || "ok");
+//     });
+    res.send(req.body);
 })
 // load defualt for redirect
 router.use("/", require(ROOT_DIR + '/controllers/default'))
