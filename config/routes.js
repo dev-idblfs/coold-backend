82c791db80172aab01dadbb0393c2c0e2ab1359f
Divyanshu <53134822+dev-idblfs@users.noreply.github.com>
2020-05-23 07:57:46 +0530

    auto  git pull and npm

--- config/routes.js
+++ config/routes.js
@@ -5,6 +5,19 @@ router.get('/sitemap.xml', function (req, res) {
     res.sendFile(`${ROOT_DIR}/public/sitemap.xml`);
 });
 
+router.get('/ok', function (req, res) {
+    const { exec } = require('child_process');
+    exec('git pull', (err, stdout, stderr) => {
+        if (err) {
+            //some err occurred
+            console.error(err)
+        } else {
+            // the *entire* stdout and stderr (buffered)
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+        }
+    });
+})
 // load defualt for redirect
 router.use("/", require(ROOT_DIR + '/controllers/default'))
 
