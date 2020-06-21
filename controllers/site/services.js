fba68c63fa098ce0d17a780c3581712b4951f99b
Divyanshu <developer.idblfs@gmail.com>
2020-06-21 11:24:25 +0530

    fix SEO

--- controllers/site/services.js
+++ controllers/site/services.js
@@ -7,7 +7,6 @@ const ejs = require('ejs');
 
 
 router.get("/", async function (req, res) {
-    console.log('aaa');
     var header = await ejs.renderFile('views/header.ejs', { css: assets.loadCSS(), base_url: CONFIG.BASE_URL });
     var footer = await ejs.renderFile('views/footer.ejs', { js: assets.loadJS(), base_url: CONFIG.BASE_URL });
     var body = await ejs.renderFile('views/service/index.ejs', { base_url: CONFIG.BASE_URL });
@@ -16,19 +15,46 @@ router.get("/", async function (req, res) {
 
 })
 
-router.get("/:service", async function (req, res) {
-    console.log(req.params);
-    var services = ['hr', 'payroll', 'performance', 'recruit', 'timesheet'];
+router.get("/hr", async function (req, res) {
 
-    if (Object.keys(req.params).length > 0 && services.includes(req.params.service)) {
-        var header = await ejs.renderFile('views/header.ejs', { css: assets.loadCSS(), base_url: CONFIG.BASE_URL });
-        var footer = await ejs.renderFile('views/footer.ejs', { js: assets.loadJS(), base_url: CONFIG.BASE_URL });
-        var body = await ejs.renderFile('views/service/aio-index.ejs', { index: services.indexOf(req.params.service) + 1, base_url: CONFIG.BASE_URL });
-        // console.log(services.indexOf(req.params.service));
-        res.render('body', { header: header, body: body, footer: footer });
-    } else {
-        res.sendStatus(404);
-    }
+    var header = await ejs.renderFile('views/header.ejs', { css: assets.loadCSS(), base_url: CONFIG.BASE_URL });
+    var footer = await ejs.renderFile('views/footer.ejs', { js: assets.loadJS(), base_url: CONFIG.BASE_URL });
+    var body = await ejs.renderFile('views/service/hr.ejs', { base_url: CONFIG.BASE_URL });
+    // console.log(services.indexOf(req.params.service));
+    res.render('body', { header: header, body: body, footer: footer });
+
+
+}); router.get("/payroll", async function (req, res) {
+
+    var header = await ejs.renderFile('views/header.ejs', { css: assets.loadCSS(), base_url: CONFIG.BASE_URL });
+    var footer = await ejs.renderFile('views/footer.ejs', { js: assets.loadJS(), base_url: CONFIG.BASE_URL });
+    var body = await ejs.renderFile('views/service/payroll.ejs', { base_url: CONFIG.BASE_URL });
+    // console.log(services.indexOf(req.params.service));
+    res.render('body', { header: header, body: body, footer: footer });
+
+}); router.get("/performance", async function (req, res) {
+    var header = await ejs.renderFile('views/header.ejs', { css: assets.loadCSS(), base_url: CONFIG.BASE_URL });
+    var footer = await ejs.renderFile('views/footer.ejs', { js: assets.loadJS(), base_url: CONFIG.BASE_URL });
+    var body = await ejs.renderFile('views/service/performance.ejs', { base_url: CONFIG.BASE_URL });
+    // console.log(services.indexOf(req.params.service));
+    res.render('body', { header: header, body: body, footer: footer });
+
+
+}); router.get("/recruit", async function (req, res) {
+    var header = await ejs.renderFile('views/header.ejs', { css: assets.loadCSS(), base_url: CONFIG.BASE_URL });
+    var footer = await ejs.renderFile('views/footer.ejs', { js: assets.loadJS(), base_url: CONFIG.BASE_URL });
+    var body = await ejs.renderFile('views/service/recruit.ejs', { base_url: CONFIG.BASE_URL });
+    // console.log(services.indexOf(req.params.service));
+    res.render('body', { header: header, body: body, footer: footer });
+
+
+}); router.get("/timesheet", async function (req, res) {
+    var header = await ejs.renderFile('views/header.ejs', { css: assets.loadCSS(), base_url: CONFIG.BASE_URL });
+    var footer = await ejs.renderFile('views/footer.ejs', { js: assets.loadJS(), base_url: CONFIG.BASE_URL });
+    var body = await ejs.renderFile('views/service/timesheet.ejs', { base_url: CONFIG.BASE_URL });
+    // console.log(services.indexOf(req.params.service));
+    res.render('body', { header: header, body: body, footer: footer });
+
 
 });
 
